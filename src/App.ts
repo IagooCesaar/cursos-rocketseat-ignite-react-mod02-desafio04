4b6e58d4a53f56487ca404076599b65e97574680
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-14 10:27:51 -0300

    inicio alteração App e index para ts

--- src/App.ts
+++ src/App.ts
@@ -1,16 +1,19 @@
+import react from 'react'
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Routes from './routes';
 
 import GlobalStyle from './styles/global';
 
-const App = () => (
-  <>
-    <GlobalStyle />
-    <Router>
-      <Routes />
-    </Router>
-  </>
-);
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes />
+      </Router>
+    </> 
+  )
+};
 
 export default App;
